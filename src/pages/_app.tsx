491b27ff7a5512bc0efb183ce32c41c985fad93d
Guilherme Oliveira <contatoguilhermedeoliveira@gmail.com>
2024-05-21 20:30:51 -0300

    improve organization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,5 +14,10 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,8 @@
-import type { DocumentContext, DocumentInitialProps } from 'next/document'
-import Document from 'next/document'
+import Document, { DocumentContext } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
