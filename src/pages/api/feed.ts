54da8cb1f452ad1e89783dfe940b7daa9e5fe9e5
Guilherme Oliveira <contatoguilhermedeoliveira@gmail.com>
2024-05-22 20:57:31 -0300

    fix api typing

--- src/pages/api/feed.ts
+++ src/pages/api/feed.ts
@@ -3,13 +3,15 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { ContentCard } from '@/modules/feed/types'
 import { feedService } from '@/modules/feed/data/service'
 
+type FeedResponse = ContentCard[] | { error: string }
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ContentCard[]>
+  res: NextApiResponse<FeedResponse>
 ) {
   try {
     const feedData = await feedService()
-    res.status(200).json(feedData)
+    res.status(200).json(feedData as ContentCard[])
   } catch (err) {
     res.status(500).json({ error: 'failed to load data' })
   }
