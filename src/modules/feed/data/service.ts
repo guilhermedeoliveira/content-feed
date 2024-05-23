50df85f8a08f56fff68d95ecb157f8312bb6d55b
Guilherme Oliveira <contatoguilhermedeoliveira@gmail.com>
2024-05-23 14:03:40 -0300

    clean up service.ts

--- src/modules/feed/data/service.ts
+++ src/modules/feed/data/service.ts
@@ -2,15 +2,6 @@ import { sortItemsByKeyDescending } from '@/lib/array'
 import { feedRepository } from './repository'
 import { ContentCard } from '../types'
 
-export const feedServicea = async () => {
-  const data = await feedRepository()
-
-  const { contentCards: cards } = data
-  const sortedCards = sortItemsByKeyDescending(cards, 'metadata.priority')
-
-  return sortedCards
-}
-
 export const feedService = async (): Promise<ContentCard[]> => {
   const data = await feedRepository()
 
